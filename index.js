9a8adbcfb0effe9adf4f899c58574ef5d707ef48
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-24 06:46:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 // Menyajikan file statis dari folder 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Menangani route utama
+// Path untuk menyimpan jumlah pengunjung
+const visitorCountFile = path.join(__dirname, 'visitor-count.txt');
+
+// API untuk menghitung jumlah pengunjung
+app.get('/api/visitor-count', (req, res) => {
+  fs.readFile(visitorCountFile, 'utf8', (err, data) => {
+    if (err) {
+      // Jika file tidak ada, buat dengan nilai 1
+      fs.writeFile(visitorCountFile, '1', (err) => {
+        if (err) throw err;
+        res.json({ visitorCount: 1 });
+      });
+    } else {
+      let visitorCount = parseInt(data);
+      visitorCount++;
+      fs.writeFile(visitorCountFile, visitorCount.toString(), (err) => {
+        if (err) throw err;
+        res.json({ visitorCount });
+      });
+    }
+  });
+});
+
+// Menyajikan file index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
